fb6febabdecb7ebe02278b475924a6227b678120
develsvai <developsvai5096@gmail.com>
2024-12-25 08:16:40 +0000

    feat : Canvas 라우팅 경로 지정

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css';
 import CanvasPage from "@/pages/Canvas/CanvasPage.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-function App() {
 
+function App() {
     return (
         <RecoilRoot>
             <BrowserRouter>
-                <Routes>
-                    <Route path="/canvas" element={<CanvasPage />} />
-                </Routes>
+                <div style={{ top: '-6px', left: '-6px', position: 'relative' }}>
+                    <Routes>
+                        <Route path="/canvas" element={<CanvasPage />} />
+                    </Routes>
+                </div>
             </BrowserRouter>
         </RecoilRoot>
     );
