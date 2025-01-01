cbe912bf11f15e2f046af983a8bf85d8dfd09dfb
develsvai <developsvai5096@gmail.com>
2025-01-01 11:23:02 +0000

    Refactor : api 파일 수정

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import userApi from "./user.api/user.api";
-import kidApi from "./user.api/kid.api";
+import kidApi from "./kid.api/kid.api";
 
 export const CanvasClient = axios.create({
     baseURL: "http://localhost:8080/",
---
+++ src/api/kid.api/index.tsx
@@ -0,0 +1,1 @@
+export {default} from "./kid.api";
